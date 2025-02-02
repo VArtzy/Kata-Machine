c5daa400e43e9bf303594022cce6f25740935fb0
VArtz <farrelnikoson@gmail.com>
2025-02-02 22:10:55 +0700

    gauss jordan elimination method

--- src/dsa/GaussJordanEliminationMethod.ts
+++ src/dsa/GaussJordanEliminationMethod.ts
@@ -1,9 +1,17 @@
-const matrix = [[2,3,4], [5,6,7], [8,9,10]]
+const matrix = [[3,-1,4,2], [17,2,1,14], [1,12,-7,54]]
 
 for(let i = 0; i < matrix.length; i++) {
-    const a = matrix[i][i]
+    const pivot = matrix[i][i]
     for(let k = 0; k < matrix[0].length; k++) {
-        matrix[i][k] = Math.floor(matrix[i][k] / a)
+        matrix[i][k] /= pivot
+    }
+    for (let j = 0; j < matrix.length; j++) {
+        if (j !== i) {
+            const factor = matrix[j][i];
+            for (let k = 0; k < matrix[0].length; k++) {
+                matrix[j][k] -= factor * matrix[i][k]
+            }
+        }
     }
 }
 
